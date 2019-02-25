0b58ee8a4172188ed2f6a30b21523cea3e17c665
zerohezitation <43836090+zerohezitation@users.noreply.github.com>
2019-02-24 17:00:59 -0800

    Create Restaurant.h

---
+++ Restaurant.h
@@ -0,0 +1,70 @@
+#ifndef RESTAURANT_H
+#define RESTAURANT_H
+
+#include "menuitem.h"
+#include "distance.h"
+#include <QString>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Restaurant
+{
+public:
+    Restaurant();
+
+    Restaurant(QString rName, int rID, double rSaddlebackDistance, int rSize, int rTotalDistance);
+
+    void SetName(QString rName);
+
+    QString GetName();
+
+    void SetID(int rID);
+
+    int GetID();
+
+    void SetSaddlebackDistance(double rSaddlebackDistance);
+
+    double GetSaddlebackDistance();
+
+    void SetSize(int rSize);
+
+    int GetSize();
+
+    void SetTotalDistance(int rTotalDistance);
+
+    int GetTotalDistance();
+
+    void SetTotalAmountSpent(double amount);
+
+    double GetTotalAmountSpent();
+
+    void SetVisitedLocation(bool rVisitedLocation);
+
+    bool GetVistedLocation();
+
+    void StoreDistancesToRestaurants(Distance distance);
+
+    vector<Distance> GetDistancesToRestaurants();
+
+    void StoreMenuItemsOfRestaurant(MenuItem menuItem);
+
+    vector<MenuItem> GetMenuItemsOfRestaurant();
+
+private:
+    QString name;
+    int     id;
+    double  saddlebackDistance;
+    int     size;
+    int     totalDistance;
+
+    double  totalSpent;
+    bool    visitedLocation;
+
+    vector<Distance> distanceList;
+    vector<MenuItem> menuItemList;
+
+};
+
+#endif // RESTAURANT_H
