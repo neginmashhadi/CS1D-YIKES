f792b4b9e3f4fbe768e5461f808f8288e1ba9c70
zerohezitation <43836090+zerohezitation@users.noreply.github.com>
2019-02-24 16:59:19 -0800

    Create Distance.cpp

---
+++ Distance.cpp
@@ -0,0 +1,45 @@
+#include "distance.h"
+
+Distance::Distance()
+{
+    miles               = 0;
+    currentRestaurantID = 0;
+    nextRestaurantID    = 0;
+}
+
+Distance::Distance(double dMiles, int currentID, int nextID)
+{
+    miles               = dMiles;
+    currentRestaurantID = currentID;
+    nextRestaurantID    = nextID;
+}
+
+void Distance::SetMiles(double dMiles)
+{
+    this->miles = dMiles;
+}
+
+double Distance::GetMiles()
+{
+    return miles;
+}
+
+void Distance::SetCurrentRestaurantID(int currentID)
+{
+    this->currentRestaurantID = currentID;
+}
+
+int Distance::GetCurrentRestaurantID()
+{
+    return currentRestaurantID;
+}
+
+void Distance::SetNextID(int nextID)
+{
+    this->nextRestaurantID = nextID;
+}
+
+int Distance::GetNextID()
+{
+    return nextRestaurantID;
+}
